030dfa63d1553a0d267d740633c8035858b6f260
Edward Fox <edwardkenfox@gmail.com>
2017-10-14 19:21:20 +0900

    Add index

--- webapp/sql/alter_table_add_index.sql
+++ webapp/sql/alter_table_add_index.sql
@@ -1,5 +1,6 @@
 ALTER TABLE relations ADD INDEX index_on_relations_one(one);
 ALTER TABLE relations ADD INDEX index_on_relations_another(another);
+ALTER TABLE relations ADD INDEX index_on_relations_created_at(created_at);
 
 ALTER TABLE profiles ADD INDEX index_on_profiles_user_id(user_id);
 ALTER TABLE entries ADD INDEX index_on_entries_user_id(user_id);
