1d94ccf42136c30852664426cc3fe797093b5c66
Hiroaki Ninomiya <me@treby.info>
2017-10-14 17:56:40 +0900

    add index

---
+++ webapp/sql/alter_table_add_index.sql
@@ -0,0 +1,7 @@
+ALTER TABLE relations ADD INDEX index_on_relations_one(one);
+ALTER TABLE relations ADD INDEX index_on_relations_another(another);
+
+ALTER TABLE profiles ADD INDEX index_on_profiles_user_id(user_id);
+ALTER TABLE entries ADD INDEX index_on_entries_user_id(user_id);
+alter table entries add index index_on_entries_private(private);
+alter table comments add index index_on_comments_created_at(created_at);
